f52896ccbcfe36d1f4b515a1fb9cc695f8ddb6e6
Tashko Pavlov <pavlovtasko@gmail.com>
2022-05-31 14:54:03 +0200

    Added AMR Graph2Graph fine-tuning pipeline.

--- fine-tune/data_interface/dataset_pl.py
+++ fine-tune/data_interface/dataset_pl.py
@@ -14,6 +14,187 @@ from torch.utils.data.dataloader import DataLoader
 from common.utils import shift_tokens_right
 
 
+class AMRGraph2Graph(pl.LightningDataModule):
+    def __init__(
+            self, tokenizer, **args,
+    ):
+        super().__init__()
+        self.train_file = args['train_data_file']
+        self.validation_file = args['eval_data_file']
+        self.test_file = args['test_data_file']
+        self.src_prefix = args['src_prefix']
+        self.tgt_prefix = args['tgt_prefix']
+        self.pad_to_max_length = False
+        self.ignore_pad_token_for_loss = True
+        self.cache_dir = args["cache_dir"]
+        self.unified_inp = args["unified_input"]
+        self.train_batchsize = args["per_gpu_train_batch_size"]
+        self.val_batchsize = args["per_gpu_eval_batch_size"]
+        self.train_num_worker = args["train_num_workers"]
+        self.val_num_worker = args["eval_num_workers"]
+        self.preprocess_worker = args["process_num_workers"]
+
+        self.tokenizer = tokenizer
+        print("Tokenizer:", len(self.tokenizer), self.tokenizer)
+        self.max_src_length = min(args["src_block_size"], self.tokenizer.model_max_length)
+        self.max_tgt_length = min(args["tgt_block_size"], self.tokenizer.model_max_length)
+
+        self.collate_fn = DataCollatorForSeq2Seq(
+            self.tokenizer,
+            label_pad_token_id=self.tokenizer.pad_token_id,
+            pad_to_multiple_of=8 if args["fp16"] else None,
+            decoder_start_token_id=self.tokenizer.bos_token_id,
+        )
+
+    def setup(self, stage="fit"):
+        data_files = {}
+
+        data_files['train'] = self.train_file
+        data_files['validation'] = self.validation_file
+        data_files['test'] = self.test_file
+
+        print("Dataset cache dir:", self.cache_dir)
+
+        datasets = load_dataset(
+            "json", data_files=data_files, cache_dir=self.cache_dir,
+        )
+
+        print("Datasets: ", datasets)
+        column_names = datasets['train'].column_names
+        print("columns: ", column_names)
+
+        def tokenize_function(examples):
+            src_amrs = examples["src"]  # amr tokens
+            tgt_amrs = examples["tgt"]  # expected_amr tokens
+            model_inputs = {}
+            if self.unified_inp:
+                amr_tokens = [
+                    [self.tokenizer.bos_token, self.tokenizer.mask_token, self.tokenizer.eos_token]
+                    + [self.tokenizer.amr_bos_token]
+                    + amr.split()
+                    + [self.tokenizer.amr_eos_token]
+                    for amr in src_amrs
+                ]
+            else:
+                amr_tokens = [
+                    [self.tokenizer.bos_token] + amr.split() + [self.tokenizer.eos_token]
+                    for amr in src_amrs
+                ]
+            src_ids = amr_batch_encode(
+                amr_tokens, max_length=self.max_src_length, pad_to_max_length=False
+            )
+            # src_mask = src_ids.ne(self.tokenizer.pad_token_id).int()
+            tgt_amr_tokens = [
+                [self.tokenizer.amr_bos_token] + amr.split() + [self.tokenizer.amr_eos_token]
+                for amr in tgt_amrs
+            ]
+            tgt_ids = amr_batch_encode(
+                tgt_amr_tokens, max_length=self.max_tgt_length, pad_to_max_length=False
+            )
+
+            label_ids = [[l for l in label[1:]] for label in tgt_ids]
+            model_inputs["input_ids"] = src_ids
+            model_inputs["labels"] = label_ids
+            return model_inputs
+        def amr_batch_encode(input_lst, max_length, pad_to_max_length=False):
+            res = []
+            for itm_lst in input_lst:
+                res.append(
+                    get_ids(itm_lst, max_length=max_length, pad_to_max_length=pad_to_max_length)
+                )
+            return res
+        def get_ids(tokens, max_length=0, pad_to_max_length=False):
+            token_ids = [self.tokenizer.encoder.get(b, self.tokenizer.unk_token_id) for b in tokens]
+            if pad_to_max_length:
+                assert max_length > 0, "Invalid max-length: {}".format(max_length)
+                pad_ids = [self.tokenizer.pad_token_id for _ in range(max_length)]
+                len_tok = len(token_ids)
+                if max_length > len_tok:
+                    pad_ids[:len_tok] = map(int, token_ids)
+                else:
+                    pad_ids = token_ids[:max_length]
+                return pad_ids
+            return token_ids
+
+        self.train_dataset = datasets["train"].map(
+            tokenize_function,
+            batched=True,
+            num_proc=self.preprocess_worker,
+            remove_columns=["src", "tgt"],
+        )
+        print(f"ALL {len(self.train_dataset)} training instances")
+        self.valid_dataset = datasets["validation"].map(
+            tokenize_function,
+            batched=True,
+            num_proc=self.preprocess_worker,
+            remove_columns=["src", "tgt"],
+        )
+        print(f"ALL {len(self.valid_dataset)} validation instances")
+
+        self.test_dataset = datasets["test"].map(
+            tokenize_function,
+            batched=True,
+            num_proc=self.preprocess_worker,
+            remove_columns=["src", "tgt"],
+        )
+        print(f"ALL {len(self.test_dataset)} test instances")
+        print("Dataset Instance Example:", self.train_dataset[0])
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.train_batchsize,
+            collate_fn=self.collate_fn,
+            shuffle=True,
+            num_workers=self.train_num_worker,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.valid_dataset,
+            batch_size=self.val_batchsize,
+            collate_fn=self.collate_fn,
+            shuffle=False,
+            num_workers=self.val_num_worker,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.val_batchsize,
+            collate_fn=self.collate_fn,
+            shuffle=False,
+            num_workers=self.val_num_worker,
+            pin_memory=True,
+        )
+
+    def load_data_module(self):
+        name = self.dataset
+        camel_name = "".join([i.capitalize() for i in name.split("_")])
+        try:
+            self.data_module = getattr(
+                importlib.import_module("." + name, package=__package__), camel_name
+            )
+        except:
+            raise ValueError(
+                f"Invalid Dataset File Name or Invalid Class Name data.{name}.{camel_name}"
+            )
+
+    def instancialize(self, **other_args):
+        """ Instancialize a model using the corresponding parameters
+            from self.hparams dictionary. You can also input any args
+            to overwrite the corresponding value in self.kwargs.
+        """
+        class_args = inspect.getargspec(self.data_module.__init__).args[1:]
+        inkeys = self.kwargs.keys()
+        args1 = {}
+        for arg in class_args:
+            if arg in inkeys:
+                args1[arg] = self.kwargs[arg]
+        args1.update(other_args)
+        return self.data_module(**args1)
+
+
 class AMR2TextDataModule(pl.LightningDataModule):
     def __init__(
         self, tokenizer, **args,
